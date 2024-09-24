3439f0cc839decc93db14046c126e1db6e17bec3
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-09-24 13:06:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+
+    }
+
+    private static int printFactors(int number) {
+        if ( number==-1 )
+
+
+
+        for(int i = 1; i <= number; i++){
+            if (0 == i);
+            System.out.print(-1);
+            System.out.println(-1);
+
+
+        }
+        return -1;
+    }
+
+}
